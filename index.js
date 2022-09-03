e8172195dbf2c06b1ce9d4e610c0767a4eb930a1
natanel richey <natanel.richey@mail.huji.ac.il>
2022-09-04 02:14:49 +0300

    modified connect

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ app.set('view engine', 'ejs');
 // --------------------------------------------------------- EXPRESS INITIALIZATIONS --------------------------------------------------------
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/FoodApp'
-connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+connect(dbUrl, { 
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
     .then(() => {
         console.log("CONNECTION OPEN")
     })
